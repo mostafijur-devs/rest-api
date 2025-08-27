273a1ad6e5a6898a5ef1d3c2779a3c83bbc74d73
Mostafijur Rahman <mostafijur.devs@gmail.com>
2025-08-27 17:28:03 +0600

    some works to ui

--- lib/google_animals/screen/animals_view.dart
+++ lib/google_animals/screen/animals_view.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'dart:convert';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:rest_api/google_animals/screen/search_screen.dart';
@@ -21,15 +23,49 @@ class _AnimalsViewState extends State<AnimalsView> {
   String _searchText = '';
 
   String get searchText => _searchController.text;
-
   bool isloading = true;
+  bool isConnected = true;
+  bool erroSearch = false;
+  StreamSubscription<List<ConnectivityResult>>? subscription;
 
   @override
   void initState() {
     super.initState();
-    _getAnimals();
+    subscription = Connectivity().onConnectivityChanged.listen((result) {
+      if (result.contains(ConnectivityResult.mobile) ||
+          result.contains(ConnectivityResult.wifi)) {
+        _getAnimals();
+        setState(() {
+          isConnected = true;
+        });
+      } else {
+        // isloading = true;
+        setState(() {
+          isConnected = false;
+          _getAnimals();
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('No internet connection'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+      }
+    });
     loadSearch();
+  }
 
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    subscription?.cancel();
+  }
+
+  Future<bool> isConectionCheck() async {
+    final result = await Connectivity().checkConnectivity();
+    return (result.contains(ConnectivityResult.mobile) ||
+        result.contains(ConnectivityResult.wifi));
   }
 
   _getAnimals() async {
@@ -47,31 +83,24 @@ class _AnimalsViewState extends State<AnimalsView> {
     } finally {
       setState(() {
         isloading = false;
+        if (googleBookResponse!.items != null) {
+          erroSearch = false;
+        } else {
+          erroSearch = true;
+        }
       });
     }
   }
 
-  //
-  // getSearchText() async {
-  //   final sharedPreferences = await SharedPreferences.getInstance();
-  //   if( searchText.isNotEmpty && searchText != ''){
-  //      await sharedPreferences.setString('searchText', searchText);
-  //     final saveText = sharedPreferences.getString('searchText')!;
-  //      setState(() {
-  //        _searchText = saveText;
-  //      });
-  //   }
-  //
-  // }
   Future<void> saveSearch({required String searchText}) async {
     final prefs = await SharedPreferences.getInstance();
     if (searchText.isNotEmpty) {
       await prefs.setString('searchText', searchText);
       setState(() {
+        isloading = true;
         loadSearch();
       });
       _searchController.clear();
-      // _searchController.clear();
       print("Saved: $searchText");
     }
   }
@@ -90,39 +119,47 @@ class _AnimalsViewState extends State<AnimalsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey.withOpacity(0.3),
       appBar: AppBar(
-        title:_searchText.isEmpty? Text('Search books'): googleBookResponse?.items?.isEmpty ?? true
+        backgroundColor: Colors.transparent.withOpacity(0.3),
+        title: _searchText.isEmpty
+            ? Text('Search books')
+            : googleBookResponse!.items!.isEmpty
             ? Text('Search not good')
-            : Text("$_searchText books List"),
+            : RichText(
+                text: TextSpan(
+                  text: _searchText.toUpperCase(),style: TextStyle(color:Colors.black,fontSize: 20,fontWeight: FontWeight.bold),
+                  children: [TextSpan(text: ' book list',style: TextStyle(color:Colors.white,))],
+                ),
+              ),
         centerTitle: true,
         actions: [
           IconButton(
             onPressed: () {
-
-              if(googleBookResponse?.items?.isEmpty ?? true){
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('No books found. Please search a valid book name.'),
-                  duration: Duration(seconds: 2),
-                ),
-              );
-
-              }else if(_searchText.isEmpty){
+              if (googleBookResponse?.items?.isEmpty ?? true) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      'No books found. Please search a valid book name.',
+                    ),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+              } else if (_searchText.isEmpty) {
                 ScaffoldMessenger.of(context).showSnackBar(
                   const SnackBar(
                     content: Text('Please search a book name.'),
                     duration: Duration(seconds: 2),
                   ),
                 );
-              }
-              else{
+              } else {
                 showSearch(
                   context: context,
-                  delegate: SearchScreen(googleBookResponse: googleBookResponse!),
+                  delegate: SearchScreen(
+                    googleBookResponse: googleBookResponse!,
+                  ),
                 );
               }
-
-
             },
             icon: const Icon(Icons.search),
           ),
@@ -145,88 +182,158 @@ class _AnimalsViewState extends State<AnimalsView> {
           // mainAxisAlignment: MainAxisAlignment.center,
           // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                hintText: 'search valued google book name',
-                labelText: 'Search',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () async {
-                    saveSearch(searchText: searchText);
-                  },
+            if (_searchText.isEmpty)
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: 'search valued google book name',
+                  labelText: 'Search',
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.search),
+                    onPressed: () async {
+                      saveSearch(searchText: searchText);
+                    },
+                  ),
                 ),
               ),
-            ),
-            isloading
+
+            googleBookResponse != null
                 ? Expanded(
-                    child: const Center(child: CircularProgressIndicator()),
+                    child: Stack(
+                      children: [
+                        Column(
+                          children: [
+                            TextField(
+                              controller: _searchController,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                hintText: 'search valued google book name',
+                                labelText: 'Search',
+                                suffixIcon: IconButton(
+                                  icon: Icon(Icons.search),
+                                  onPressed: () async {
+                                    saveSearch(searchText: searchText);
+                                  },
+                                ),
+                              ),
+                            ),
+
+                            if (!erroSearch && !isloading)
+                              Expanded(
+                                child: ListView.builder(
+                                  itemCount:
+                                      googleBookResponse?.items?.length ?? 0,
+                                  itemBuilder: (context, index) {
+                                    final book = googleBookResponse
+                                        ?.items?[index]
+                                        .volumeInfo;
+
+                                    return Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: ListTile(
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => AnimalView(
+                                                volumeInfo: book,
+                                                googleBookResponse:
+                                                    googleBookResponse!,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                        title: Text(book?.title ?? 'No Title'),
+                                        subtitle: Text(
+                                          (book?.authors != null &&
+                                                  book!.authors!.isNotEmpty)
+                                              ? book.authors!.join(", ")
+                                              : "Unknown Author",
+                                        ),
+                                        leading: CachedNetworkImage(
+                                          imageUrl:
+                                              book?.imageLinks?.thumbnail ?? '',
+                                          placeholder: (context, url) =>
+                                              const CircularProgressIndicator(),
+                                          errorWidget: (context, url, error) =>
+                                              const Icon(Icons.broken_image),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            if (isloading)
+                              Expanded(
+                                child: Container(
+                                  child: const Center(
+                                    child: CircularProgressIndicator(),
+                                  ),
+                                ),
+                              ),
+                            if (erroSearch)
+                              Expanded(
+                                child: Center(
+                                  child: Text('Your Search book not found'),
+                                ),
+                              ),
+                          ],
+                        ),
+
+                        if (!isConnected)
+                          Container(
+                            height: double.infinity,
+                            width: double.infinity,
+                            color: Colors.black.withOpacity(0.7),
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(15),
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 30,
+                                  color: Colors.black.withOpacity(0.5),
+                                  child: Center(
+                                    child: Text(
+                                      'Internet connection is failed',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                      ],
+                    ),
                   )
                 : _searchText.isEmpty
                 ? Expanded(
-                    child: Center(child: Text(' Please search your book ')),
+                    child: Center(child: Text('Please search your book')),
                   )
-                : googleBookResponse?.items?.isEmpty ?? true
+                : !isConnected
                 ? Expanded(
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'No books found',
-                            style: TextStyle(fontSize: 20, color: Colors.red),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Center(
+                          child: Text(
+                            'Internet connection fail.. \nPlease check your internet connection',
                           ),
-                          Text(
-                            'Your search was  ( $_searchText ) not valid',
-                            style: TextStyle(),
-                          ),
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: 20),
+                        CircularProgressIndicator(),
+                      ],
                     ),
                   )
-                : Expanded(
-                    child: ListView.builder(
-                      itemCount: googleBookResponse?.items?.length ?? 0,
-                      itemBuilder: (context, index) {
-                        final book =
-                            googleBookResponse?.items?[index].volumeInfo;
-
-                        return Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ListTile(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => AnimalView(
-                                    volumeInfo: book,
-                                    googleBookResponse: googleBookResponse!,
-                                  ),
-                                ),
-                              );
-                            },
-                            title: Text(book?.title ?? 'No Title'),
-                            subtitle: Text(
-                              (book?.authors != null &&
-                                      book!.authors!.isNotEmpty)
-                                  ? book.authors!.join(", ")
-                                  : "Unknown Author",
-                            ),
-                            leading: CachedNetworkImage(
-                              imageUrl: book?.imageLinks?.thumbnail ?? '',
-                              placeholder: (context, url) =>
-                                  const CircularProgressIndicator(),
-                              errorWidget: (context, url, error) =>
-                                  const Icon(Icons.broken_image),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
+                : isloading
+                ? Expanded(
+                    child: const Center(child: CircularProgressIndicator()),
+                  )
+                : Center(child: CircularProgressIndicator()),
           ],
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 import 'dragonball/screen/dragon_ball_character_sacreen.dart';
 import 'google_animals/screen/animals_view.dart';
 
-void main (){
+void main() {
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -16,8 +16,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AnimalsView(),
-      builder:EasyLoading.init() ,
+      home: AnimatedSplashScreen(
+        nextScreen: AnimalsView(),
+        splash: Image.asset(
+          'assets/images/img.png',
+          height: 600,
+          width: 200,
+          fit: BoxFit.contain,
+          cacheHeight: 300,
+          // color:Colors.transparent,
+        ),
+        backgroundColor: Colors.blueGrey.withOpacity(0.5),
+        splashTransition: SplashTransition.scaleTransition,
+      ),
+      builder: EasyLoading.init(),
     );
   }
 }
